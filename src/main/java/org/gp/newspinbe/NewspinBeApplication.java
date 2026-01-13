cce34bda8e4fb053ab83d908e4b4ad4cc79e47d2
sohyeonjung <jhyeon23@konkuk.ac.kr>
2026-01-13 20:36:23 +0900

    Initial

---
+++ src/main/java/org/gp/newspinbe/NewspinBeApplication.java
@@ -0,0 +1,13 @@
+package org.gp.newspinbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspinBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspinBeApplication.class, args);
+	}
+
+}
